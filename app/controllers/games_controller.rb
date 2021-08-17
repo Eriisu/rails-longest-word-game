1488d013ab6a5e99e541e55437a77fe167cafcf6
Elise Burridge <emjburridge@gmail.com>
2021-08-17 08:41:24 +0100

    Built working game

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,36 @@
+require 'open-uri'
+
+class GamesController < ApplicationController
+  VOWELS = ["a", "e", "i", "o", "u"]
+  def new
+    @letters = Array.new(5) { VOWELS.sample }
+    @letters += Array.new(5) { (('a'..'z').to_a - VOWELS).sample }
+    @letters.shuffle!
+    @time = Time.now
+  end
+
+  def score
+    @start_time = params[:time].to_i
+    @end_time = Time.now.to_i
+    @time_length = @end_time - @start_time
+    @user_answer = params[:answer]
+    @random_letters = params[:random_letters]
+    @included = included?
+    @english_word = english_word?
+    @score = (1 / @time_length.to_f) * @user_answer.length
+    # @score = 1/if @included && @english_word
+  end
+
+  private
+
+  def included?
+    @user_answer.chars.all? { |letter| @user_answer.count(letter) <= @random_letters.count(letter) }
+  end
+
+  def english_word?
+    url = "https://wagon-dictionary.herokuapp.com/#{@user_answer}"
+    response = URI.open(url).read
+    result = JSON.parse(response)
+    result['found']
+  end
+end
